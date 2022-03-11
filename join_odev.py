5973c26b02e507713f708f14ca0ea5619b01e79a
karagulruhi <79934031+karagulruhi@users.noreply.github.com>
2022-03-11 15:34:58 +0300

    Update join_odev.py

--- join_odev.py
+++ join_odev.py
@@ -1,5 +1,4 @@
-
-
+#ödev 9
 
 
 soru 1
@@ -24,4 +23,32 @@ soru 3
 SELECT rental_id, first_name, last_name
 FROM customer
 INNER JOIN rental
-ON customer.customer_id=rental.customer_id;
+ON customer.customer_id=rental.customer_id
+
+
+#ödev 10
+
+soru 1 
+
+SELECT city, country
+FROM city
+LEFT JOIN country
+ON city.country_id = country.country_id
+
+
+soru 2 
+
+SELECT city, country
+FROM city
+LEFT JOIN country
+ON city.country_id = country.country_id
+
+soru 3 
+
+SELECT rental_id, first_name, last_name
+FROM customer
+FULL JOIN rental
+ON customer.customer_id = rental.customer_id
+
+
+
