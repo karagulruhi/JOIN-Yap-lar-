cbd93c5cc40d6cc8d7ca338c1c3a90fa8236c9a5
karagulruhi <Karagulruhii@gmail.com>
2022-03-11 15:17:20 +0300

    firsty

---
+++ join_odev.py
@@ -0,0 +1,27 @@
+
+
+
+
+soru 1
+
+SELECT city, country 
+FROM city 
+INNER JOIN country 
+ON city.country_id=country.country_id;
+
+soru 2
+
+
+SELECT payment_id, first_name, last_name
+FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id;
+
+
+soru 3 
+
+
+SELECT rental_id, first_name, last_name
+FROM customer
+INNER JOIN rental
+ON customer.customer_id=rental.customer_id;
